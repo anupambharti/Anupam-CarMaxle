2802c1a32e2c100a203dcf3cb70f5e0d6f2eabc2
Anu <anupambhartiab@gmail.com>
2024-12-09 07:54:19 +0530

    Initial Commit

---
+++ Anupam-CarMaxle/Anupam_CarMaxleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anupam_CarMaxleApp.swift
+//  Anupam-CarMaxle
+//
+//  Created by Anu on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Anupam_CarMaxleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Anupam-CarMaxle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Anupam-CarMaxle
+//
+//  Created by Anu on 09/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
